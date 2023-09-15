ffecd00a58b06008c9ccdd1acb3848b2d8196e3a
JesseKind <d279759@edu.rocwb.nl>
2023-09-15 16:14:37 +0200

    Added extra book list, extra button, book css updated

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,5 +1,6 @@
 export interface Book {
-    id: number;
-    name: string;
-    description: string;
-  }
+  id: number;
+  name: string;
+  description: string;
+  isRead: boolean; // New property to track if the book has been read
+}
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -9,12 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent {
-  books = BOOKS;
+  books: Book[] = [];
+  readBooks: Book[] = [];
   newBookName: string = '';
   newBookDescription: string = '';
+  newReadBookName: string = ''; // New input for read books
+  newReadBookDescription: string = ''; // New input for read books
   selectedBook?: Book;
 
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    // Retrieve the state from localStorage if it exists
+    const storedBooks = localStorage.getItem('books');
+    const storedReadBooks = localStorage.getItem('readBooks');
+
+    if (storedBooks) {
+      this.books = JSON.parse(storedBooks);
+    } else {
+      // If no books are in local storage, use the initial BOOKS array
+      this.books = [...BOOKS];
+    }
+
+    if (storedReadBooks) {
+      this.readBooks = JSON.parse(storedReadBooks);
+    }
+  }
 
   onSelect(book: Book): void {
     this.selectedBook = book;
@@ -23,17 +41,69 @@ export class BookComponent {
   addBook(): void {
     if (this.newBookName.trim() !== '') {
       const newBook: Book = {
-        id: this.books.length + 1, // Generate a new ID
+        id: Math.floor(Math.random() * Date.now()), // Generate a random number based on the current timestamp
         name: this.newBookName,
         description: this.newBookDescription,
+        isRead: true,
       };
-  
-      // Push the new book to the books array
+
+      // Check if the generated ID already exists and regenerate if needed
+      while (this.books.some(book => book.id === newBook.id)) {
+        newBook.id = Math.floor(Math.random() * Date.now());
+      }
+
       this.books.push(newBook);
-  
-      // Clear the input field
+
+      // Update the local storage to persist the state
+      localStorage.setItem('books', JSON.stringify(this.books));
+      localStorage.setItem('readBooks', JSON.stringify(this.readBooks));
+
       this.newBookName = '';
       this.newBookDescription = '';
+    }
+  }
+
+  markAsRead(book: Book): void {
+    book.isRead = true;
+    this.readBooks.push(book);
+    this.books = this.books.filter(b => b !== book);
+
+    // Update the local storage to persist the state
+    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem('readBooks', JSON.stringify(this.readBooks));
+  }
+
+  moveToBooks(book: Book): void {
+    book.isRead = false;
+    this.books.push(book);
+    this.readBooks = this.readBooks.filter(b => b !== book);
+
+    // Update the local storage to persist the state
+    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem('readBooks', JSON.stringify(this.readBooks));
+  }
+
+  addReadBook(): void {
+    if (this.newReadBookName.trim() !== '') {
+      const newReadBook: Book = {
+        id: Math.floor(Math.random() * Date.now()), // Generate a random number based on the current timestamp
+        name: this.newReadBookName,
+        description: this.newReadBookDescription,
+        isRead: true,
+      };
+
+      // Check if the generated ID already exists and regenerate if needed
+      while (this.readBooks.some(book => book.id === newReadBook.id)) {
+        newReadBook.id = Math.floor(Math.random() * Date.now());
+      }
+
+      this.readBooks.push(newReadBook);
+
+      // Update the local storage to persist the state
+      localStorage.setItem('readBooks', JSON.stringify(this.readBooks));
+
+      this.newReadBookName = '';
+      this.newReadBookDescription = '';
     }
   }
 }
